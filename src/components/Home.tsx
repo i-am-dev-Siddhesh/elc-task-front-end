db4528d2224b865928d15feb2b133f1b6cf63c93
i-am-dev-Siddhesh <shindesiddhesh.co.in@gmail.com>
2023-01-05 10:30:52 +0530

    feat: initial code

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="content">
+        <p>ELC Coding Test...</p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
