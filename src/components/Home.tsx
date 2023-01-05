50b6df753d9d298e7ea7556fe3258a948aa368e0
i-am-dev-Siddhesh <shindesiddhesh.co.in@gmail.com>
2023-01-05 18:09:45 +0530

    feat: inline css converted to scss

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,58 +1,43 @@
 import { IData } from "../types/index";
 import ClipLoader from "react-spinners/ClipLoader";
+
 interface IProps {
   data: IData[];
   isLoading: boolean;
 }
+
 const Home = ({ data, isLoading }: IProps) => {
   return (
     <section id="home">
-      <div className="content">
-        <p>ELC Coding Test...</p>
-      </div>
-
       {isLoading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-          }}
-        >
+        <div className="loader">
           <ClipLoader size="200px" color="white" />
         </div>
       ) : (
-        <div
-          style={{
-            display: "flex",
-            gap: "5px",
-            flexWrap: "wrap",
-            justifyContent: "center",
-          }}
-        >
+        <div className="container">
           {data &&
             data?.map((item) => {
               return (
-                <div
-                  key={item._id}
-                  style={{
-                    background: "white",
-                    border: "1px solid red",
-                    padding: "10px",
-                    maxWidth: "300px",
-                    gap: "10px",
-                  }}
-                >
-                  <p>{item._id}</p>
-                  <p>{item.isActive}</p>
-                  <p>{item.price}</p>
-                  <p>{item.name}</p>
-                  <p>{item.about}</p>
-                  <img src={item.picture} alt={item.name}></img>
-                  <div>
-                    {item.tags.map((tag) => (
-                      <p>{tag}</p>
+                <div key={item._id} className="card">
+                  <div className="card-top-bar">
+                    <p className="id">ID: {item._id}</p>
+                    <p className="price">Price: {item.price}</p>
+                    <p className="isActive">{item.isActive}</p>
+                  </div>
+
+                  <img
+                    src={item.picture}
+                    alt={item.name}
+                    height={300}
+                    width="100%"
+                  ></img>
+
+                  <p className="name">Name: {item.name}</p>
+                  <p className="about">{item.about}</p>
+
+                  <div className="tag-container">
+                    {item?.tags?.map((tag) => (
+                      <p key={tag}>#{tag}</p>
                     ))}
                   </div>
                 </div>
